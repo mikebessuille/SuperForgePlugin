84c55808003df3a86307e5a01add0df63cdea452
Mike Bessuille <mikebessuille@hotmail.com>
2015-02-23 16:37:43 -0500

    Fixed a bunch of bugs
    Velocity of entity when hit
    Avoiding sending 2 fireballs
    Avoid sending fireball when right-click a block (side or top)

--- src/main/java/Henry/Bessuille/Minecraft/items/HenryWand.java
+++ src/main/java/Henry/Bessuille/Minecraft/items/HenryWand.java
@@ -7,7 +7,9 @@ import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.projectile.EntityLargeFireball;
 import net.minecraft.item.Item;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.ItemFishingRod;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.MovingObjectPosition;
 import net.minecraft.util.Vec3;
 import net.minecraft.world.World;
 import net.minecraft.block.Block;
@@ -40,7 +42,6 @@ public class HenryWand extends Item
         {
             if( face != 1)
             {
-                // TODO:  Something more interesting if clicking the side of a block
                 // item was not clicked on the top of a block
                 if (!player.canPlayerEdit(x, y, z, face, tool))
                 {
@@ -51,7 +52,6 @@ public class HenryWand extends Item
             else
             {
                 // Face is Top of block
-                // TODO:  Do something more interesting if clicking on a block
                 EntityChicken chicken = new EntityChicken( world );
                 chicken.setPosition( x + 0.5, y + 1, z + 0.5 );
                 world.spawnEntityInWorld( chicken );
@@ -62,29 +62,42 @@ public class HenryWand extends Item
     }
 
 
-    // onItemRightClick seems to be called only when the item is clicked into the air (not on a block)
+    // onItemRightClick is called whenever the item is right-clicked (whether it's on a block, or not!)
     @Override
     public ItemStack onItemRightClick( ItemStack stack, World world, EntityPlayer player )
     {
         if( !world.isRemote )
         {
-            // SuperForgePlugin.getLogger().info( "WAND: RIGHT CLICK");
-            // TODO:  Why does this also get called when I'm clicking on a block?
-            // TODO:  spawn a new type of black fireball
+            // This checks whether we right-clicked on air instead of a block or entity
+            MovingObjectPosition pos = getMovingObjectPositionFromPlayer( world, player, true );
+            if( pos == null || ( pos.typeOfHit != MovingObjectPosition.MovingObjectType.BLOCK &&
+                                 pos.typeOfHit != MovingObjectPosition.MovingObjectType.ENTITY ))
+            {
+                // if we didn't right-click on a block
 
-            Vec3 look = player.getLookVec();
-            EntityLargeFireball fireball = new EntityLargeFireball(world, player, 0, 0, 0);
-            fireball.setPosition(
-                    player.posX + look.xCoord * 2,
-                    player.posY + look.yCoord * 2,
-                    player.posZ + look.zCoord * 2);
-            fireball.accelerationX = look.xCoord * 0.4;
-            fireball.accelerationY = look.yCoord * 0.4;
-            fireball.accelerationZ = look.zCoord * 0.4;
-            world.spawnEntityInWorld(fireball);
+                // SuperForgePlugin.getLogger().info( "WAND: RIGHT CLICK");
+                // TODO:  spawn a new type of black fireball
+
+                Vec3 look = player.getLookVec();
+                EntityLargeFireball fireball = new EntityLargeFireball(world, player, 0, 0, 0);
+                fireball.setPosition(
+                        player.posX + look.xCoord * 2,
+                        player.posY + look.yCoord * 2,
+                        player.posZ + look.zCoord * 2);
+                fireball.accelerationX = look.xCoord * 0.6;
+                fireball.accelerationY = look.yCoord * 0.6;
+                fireball.accelerationZ = look.zCoord * 0.6;
+                world.spawnEntityInWorld(fireball);
+            }
+            else
+            {
+                // Clicked on block or entity.  This is handled in onItemUse()
+            }
         }
-        return( stack );
+        // return( stack );
+        return( super.onItemRightClick( stack, world, player ));
     }
+
 
     @Override
     public boolean onLeftClickEntity( ItemStack tool, EntityPlayer player, Entity entity)
@@ -94,11 +107,14 @@ public class HenryWand extends Item
         {
             entity.setFire( 10 );
             Vec3 look = player.getLookVec();
-            entity.setVelocity( look.xCoord * 3, look.yCoord * 3, 2.0 );
+            // the direction the player is looking.  x, z are map coords, and y is height.
+            // This makes the entity always jump up a little when hit.
+            entity.setVelocity( look.xCoord, 1.0, look.zCoord );
         }
 
         // if we return true, the entity won't be attacked
-        return false;
+        // return false;
+        return super.onLeftClickEntity( tool, player, entity );
     }
 
 }
