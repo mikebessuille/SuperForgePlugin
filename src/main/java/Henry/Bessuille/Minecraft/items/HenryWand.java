8425b2cd6e5a8bd392cca3a140aaf4f2230408b5
Mike Bessuille <mikebessuille@hotmail.com>
2015-02-25 12:19:20 -0500

    Lightning Works!
    getTarget method works!! Adjusts for server vs. client.  Lightning
    works (without also spawning fireball) and works on both client and
    server.

--- src/main/java/Henry/Bessuille/Minecraft/items/HenryWand.java
+++ src/main/java/Henry/Bessuille/Minecraft/items/HenryWand.java
@@ -1,20 +1,26 @@
 package Henry.Bessuille.Minecraft.items;
 
 import Henry.Bessuille.Minecraft.SuperForgePlugin;
+import cpw.mods.fml.common.FMLCommonHandler;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLiving;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.passive.EntityChicken;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.entity.projectile.EntityLargeFireball;
+import net.minecraft.entity.effect.EntityLightningBolt;
 import net.minecraft.item.Item;
 import net.minecraft.creativetab.CreativeTabs;
-import net.minecraft.item.ItemFishingRod;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.MathHelper;
 import net.minecraft.util.MovingObjectPosition;
 import net.minecraft.util.Vec3;
 import net.minecraft.world.World;
 import net.minecraft.block.Block;
 import net.minecraft.block.*;
 import net.minecraft.init.Blocks;
+
 
 /**
  * Created by mbessuille on 15-02-16.
@@ -66,32 +72,48 @@ public class HenryWand extends Item
     @Override
     public ItemStack onItemRightClick( ItemStack stack, World world, EntityPlayer player )
     {
-        if( !world.isRemote )
+        // TODO: Don't attack with lightning if this is also a "use" event (close to the player!)
+        EntityLivingBase entityTarget = ModItems.getTarget( world, player, 200.0D );
+        if( entityTarget != null )
         {
-            // This checks whether we right-clicked on air instead of a block or entity
-            MovingObjectPosition pos = getMovingObjectPositionFromPlayer( world, player, true );
-            if( pos == null || ( pos.typeOfHit != MovingObjectPosition.MovingObjectType.BLOCK &&
-                                 pos.typeOfHit != MovingObjectPosition.MovingObjectType.ENTITY ))
+            // hit an entity, so spawn Lightning (want to do this on both server and client)
+            EntityLightningBolt bolt = new EntityLightningBolt( world, entityTarget.posX, entityTarget.posY, entityTarget.posZ );
+            world.spawnEntityInWorld( bolt );
+        }
+        else
+        {
+            if (!world.isRemote)  // Only spawn most entities on the server, not both client and server!
             {
-                // if we didn't right-click on a block
+                // This checks whether we right-clicked on air instead of a block
+                // Base version of this method only checks out to 5 blocks; otherwise its return is NULL
+                MovingObjectPosition pos = getMovingObjectPositionFromPlayer(world, player, true);
+                // Long distance version never returns ENTITY
+                // MovingObjectPosition pos = ModItems.getPositionLongDistance( world, player, true );
 
-                // SuperForgePlugin.getLogger().info( "WAND: RIGHT CLICK");
-                // TODO:  spawn a new type of black fireball
+                if (pos == null || pos.typeOfHit != MovingObjectPosition.MovingObjectType.BLOCK)
+                {
+                    // if we didn't right-click on a block
 
-                Vec3 look = player.getLookVec();
-                EntityLargeFireball fireball = new EntityLargeFireball(world, player, 0, 0, 0);
-                fireball.setPosition(
-                        player.posX + look.xCoord * 2,
-                        player.posY + look.yCoord * 2,
-                        player.posZ + look.zCoord * 2);
-                fireball.accelerationX = look.xCoord * 0.6;
-                fireball.accelerationY = look.yCoord * 0.6;
-                fireball.accelerationZ = look.zCoord * 0.6;
-                world.spawnEntityInWorld(fireball);
-            }
-            else
-            {
-                // Clicked on block or entity.  This is handled in onItemUse()
+                    // SuperForgePlugin.getLogger().info( "WAND: RIGHT CLICK");
+                    // TODO:  spawn a new type of black fireball
+
+                    Vec3 look = player.getLookVec();
+                    EntityLargeFireball fireball = new EntityLargeFireball(world, player, 0, 0, 0);
+                    fireball.setPosition(
+                            player.posX + look.xCoord * 2,
+                            player.posY + look.yCoord * 2,
+                            player.posZ + look.zCoord * 2);
+                    // Not sure why, but setVelocity doesn't do the same thing, and there's no
+                    // setacceleration method
+                    fireball.accelerationX = look.xCoord * 0.6;
+                    fireball.accelerationY = look.yCoord * 0.6;
+                    fireball.accelerationZ = look.zCoord * 0.6;
+                    world.spawnEntityInWorld(fireball);
+                }
+                else
+                {
+                    // Clicked on nearby block.  This is handled in onItemUse()
+                }
             }
         }
         // return( stack );
@@ -103,6 +125,7 @@ public class HenryWand extends Item
     public boolean onLeftClickEntity( ItemStack tool, EntityPlayer player, Entity entity)
     {
         // When this item is used to attack an entity, also light it on fire and throw it away from the player
+        // Consider overriding the hitEntity() method instead??
         if( !entity.isImmuneToFire() && !entity.isBurning())
         {
             entity.setFire( 10 );
@@ -117,4 +140,31 @@ public class HenryWand extends Item
         return super.onLeftClickEntity( tool, player, entity );
     }
 
+
+    // This only gets called if the player right-clicks on an entity that is close to him
+    // Need to use onItemRightClick to check if the user is looking at an entity
+    // TODO: Make this do something different if clicking close, versus how it's handled in onItemRightClick()
+    @Override
+    public boolean itemInteractionForEntity( ItemStack stack, EntityPlayer player, EntityLivingBase entity )
+    {
+        World world = player.worldObj;
+        // Do this on both client and server, because we must spawn lightning on both
+        // (Most entities are only spawned on the server)
+
+        // Vec3 loc = entity.getPosition();   // loc.xCoord, loc.yCoord, loc.zCoord
+        // Block target = findBlockUnderEntity( entity );
+        int x = MathHelper.floor_double(entity.posX);
+        int y = MathHelper.floor_double(entity.boundingBox.minY)-1;  // this would get the block BELOW the entity
+        // int y = MathHelper.floor_double(entity.posY);
+        int z = MathHelper.floor_double(entity.posZ);
+        // if( world.canLightningStrikeAt( x, y, z )) // This fails if it's not raining!
+        // TODO:  Try this without the check for sky; does this work underground?
+        if( world.canBlockSeeTheSky( x, y+1, z ))
+        {
+            EntityLightningBolt bolt = new EntityLightningBolt( world, x, y, z );
+            world.spawnEntityInWorld( bolt );
+        }
+        return true;
+        // return( super.itemInteractionForEntity( stack, entity );
+    }
 }
--- src/main/java/Henry/Bessuille/Minecraft/items/ModItems.java
+++ src/main/java/Henry/Bessuille/Minecraft/items/ModItems.java
@@ -1,8 +1,26 @@
 package Henry.Bessuille.Minecraft.items;
 
-import net.minecraft.item.Item;
-
+import cpw.mods.fml.common.FMLCommonHandler;
 import cpw.mods.fml.common.registry.GameRegistry;
+
+import net.minecraft.block.Block;
+import net.minecraft.client.Minecraft;
+import net.minecraft.entity.item.EntityItemFrame;
+import net.minecraft.item.Item;
+import net.minecraft.util.AxisAlignedBB;
+import net.minecraft.util.MathHelper;
+import net.minecraft.util.MovingObjectPosition;
+import net.minecraft.util.Vec3;
+import net.minecraft.world.World;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLiving;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraftforge.client.MinecraftForgeClient;
+import net.minecraft.util.MovingObjectPosition;
+
+import java.util.List;
+
 
 /**
  * Created by mbessuille on 15-02-19.
@@ -13,5 +31,151 @@ public class ModItems
     {
         Item referenceItem = new HenryWand();
         GameRegistry.registerItem( referenceItem, referenceItem.getUnlocalizedName() );
+    }
+
+    // Utility class with extended distance.
+    // TODO:  Make this support hitting entities!  Or delete this if getTarget() works!
+    protected static MovingObjectPosition getPositionLongDistance(World world, EntityPlayer player, boolean p_77621_3_)
+    {
+        float f = 1.0F;
+        float f1 = player.prevRotationPitch + (player.rotationPitch - player.prevRotationPitch) * f;
+        float f2 = player.prevRotationYaw + (player.rotationYaw - player.prevRotationYaw) * f;
+        double d0 = player.prevPosX + (player.posX - player.prevPosX) * (double)f;
+        double d1 = player.prevPosY + (player.posY - player.prevPosY) * (double)f + (double)(world.isRemote ? player.getEyeHeight() - player.getDefaultEyeHeight() : player.getEyeHeight());
+        // isRemote check to revert changes to ray trace position due to adding the eye height clientside and player yOffset differences
+        double d2 = player.prevPosZ + (player.posZ - player.prevPosZ) * (double)f;
+        Vec3 vec3 = Vec3.createVectorHelper(d0, d1, d2);
+        float f3 = MathHelper.cos(-f2 * 0.017453292F - (float) Math.PI);
+        float f4 = MathHelper.sin(-f2 * 0.017453292F - (float)Math.PI);
+        float f5 = -MathHelper.cos(-f1 * 0.017453292F);
+        float f6 = MathHelper.sin(-f1 * 0.017453292F);
+        float f7 = f4 * f5;
+        float f8 = f3 * f5;
+        double d3 = 300.0D;
+        Vec3 vec31 = vec3.addVector((double)f7 * d3, (double)f6 * d3, (double)f8 * d3);
+        return world.func_147447_a(vec3, vec31, p_77621_3_, !p_77621_3_, false);
+    }
+
+
+    // Utility function found on web for getting a target at a distance
+    protected static EntityLivingBase getTarget(World world, EntityPlayer player, double distance)
+    {
+        final float par1 = 1.0F; // partialtick param, something to do with current vs. previous locations?
+        // Minecraft mc = Minecraft.getMinecraft();
+
+
+        final double EYE_HEIGHT = 1.62;  // this is a guess... need to confirm it
+
+        /* FROM internet fix
+        Vec3 startPos = this.getPosition(partialTick);
+        if (!world.isRemote) startPos = startPos.addVector(0, EYE_HEIGHT, 0);
+        Vec3 look = this.getLook(partialTick);
+        Vec3 endPos = vec3.addVector(look.xCoord * reachDistance, look.yCoord * reachDistance, look.zCoord * reachDistance);
+        MovingObjectPosition mop = world.func_147447_a(startPos, endPos, false, false, true);
+
+        System.out.println("HitVec:[" + mop.hitVec.xCoord + ", " + mop.hitVec.yCoord + ", " + mop.hitVec.zCoord + "]");
+        */
+
+
+        Entity pointedEntity;
+        double d0 = distance;
+        // MovingObjectPosition omo = mc.renderViewEntity.rayTrace(d0, par1);
+        // MRB:  replaced with this line:
+        MovingObjectPosition omo = player.rayTrace(d0, par1);
+        double d1 = d0;
+        // Vec3 startPos = mc.renderViewEntity.getPosition(par1);
+        // Vec3 lookPos = mc.renderViewEntity.getLook(par1);
+        Vec3 startPos = player.getPosition(par1);
+        // Account for difference between server and client.
+        if (!world.isRemote) startPos = startPos.addVector(0, EYE_HEIGHT, 0);
+        Vec3 lookPos = player.getLook(par1);
+        Vec3 vec32 = startPos.addVector(lookPos.xCoord * d0, lookPos.yCoord * d0, lookPos.zCoord * d0);
+        pointedEntity = null;
+        Vec3 vec33 = null;
+        float f1 = 1.0F;
+        // List list = mc.theWorld.getEntitiesWithinAABBExcludingEntity(mc.renderViewEntity, mc.renderViewEntity.boundingBox.addCoord(lookPos.xCoord * d0, lookPos.yCoord * d0, lookPos.zCoord * d0).expand((double)f1, (double)f1, (double)f1));
+        List list = world.getEntitiesWithinAABBExcludingEntity( player,
+                                                                player.boundingBox.addCoord( lookPos.xCoord * d0,
+                                                                                             lookPos.yCoord * d0,
+                                                                                             lookPos.zCoord * d0).expand((double)f1,
+                                                                                             (double)f1, (double)f1));
+        double d2 = d1;
+
+        for (int i = 0; i < list.size(); ++i)
+        {
+            Entity entity = (Entity)list.get(i);
+
+            if (entity.canBeCollidedWith())
+            {
+                float f2 = entity.getCollisionBorderSize();
+                AxisAlignedBB axisalignedbb = entity.boundingBox.expand((double)f2, (double)f2, (double)f2);
+                MovingObjectPosition movingobjectposition = axisalignedbb.calculateIntercept(startPos, vec32);
+
+                if (axisalignedbb.isVecInside(startPos))
+                {
+                    if (0.0D < d2 || d2 == 0.0D)
+                    {
+                        pointedEntity = entity;
+                        vec33 = movingobjectposition == null ? startPos : movingobjectposition.hitVec;
+                        d2 = 0.0D;
+                    }
+                }
+                else if (movingobjectposition != null)
+                {
+                    double d3 = startPos.distanceTo(movingobjectposition.hitVec);
+
+                    if (d3 < d2 || d2 == 0.0D)
+                    {
+                        if (entity == player.ridingEntity && !entity.canRiderInteract())
+                        {
+                            if (d2 == 0.0D)
+                            {
+                                pointedEntity = entity;
+                                vec33 = movingobjectposition.hitVec;
+                            }
+                        }
+                        else
+                        {
+                            pointedEntity = entity;
+                            vec33 = movingobjectposition.hitVec;
+                            d2 = d3;
+                        }
+                    }
+                }
+            }
+        }
+        if (pointedEntity != null && (d2 < d1 || omo == null))
+        {
+            omo = new MovingObjectPosition(pointedEntity, vec33);
+
+            if (pointedEntity instanceof EntityLivingBase || pointedEntity instanceof EntityItemFrame)
+            {
+                // MRB: Not sure what this does or if it's necessary...
+                // MRB: I moved the mc= line to here from the top, as I'm not using mc anywhere else.
+                Minecraft mc = Minecraft.getMinecraft();
+                mc.pointedEntity = pointedEntity;
+            }
+        }
+        if (omo != null)
+        {
+            if (omo.typeOfHit == MovingObjectPosition.MovingObjectType.ENTITY)
+            {
+                if(omo.entityHit instanceof EntityLivingBase)
+                {
+
+                    return (EntityLivingBase)omo.entityHit;
+                }
+            }
+        }
+        return null;
+    }
+
+
+    static protected Block findBlockUnderEntity(Entity parEntity)
+    {
+        int blockX = MathHelper.floor_double(parEntity.posX);
+        int blockY = MathHelper.floor_double(parEntity.boundingBox.minY)-1;
+        int blockZ = MathHelper.floor_double(parEntity.posZ);
+        return parEntity.worldObj.getBlock(blockX, blockY, blockZ);
     }
 }
